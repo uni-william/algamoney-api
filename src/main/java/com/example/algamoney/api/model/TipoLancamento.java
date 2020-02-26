025202a10616ae9d9a3b08328be889b383a81796
William Cassio <william.desenv@gmail.com>
2020-02-25 21:48:23 -0400

    Adicionando relatórios

--- src/main/java/com/example/algamoney/api/model/TipoLancamento.java
+++ src/main/java/com/example/algamoney/api/model/TipoLancamento.java
@@ -2,6 +2,17 @@ package com.example.algamoney.api.model;
 
 public enum TipoLancamento {
 
-	RECEITA,
-	DESPESA
+	RECEITA("Receita"),
+	DESPESA("Despesa");
+
+	private final String descricao;
+
+	TipoLancamento(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
 }
